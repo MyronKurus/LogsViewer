acae826ab79db56ee1c83d05c23ccb1605bae538
Myron Kurus <myron.kurus@gmail.com>
2018-02-13 14:03:37 +0200

    first commit

--- src/app/components/date-input/date-input.component.ts
+++ src/app/components/date-input/date-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 const equals = (one: NgbDateStruct, two: NgbDateStruct) =>
   one && two && two.year === one.year && two.month === one.month && two.day === one.day;
@@ -18,32 +19,35 @@ const after = (one: NgbDateStruct, two: NgbDateStruct) =>
   styleUrls: ['./date-input.component.css']
 })
 export class DateInputComponent {
-    model;
-    private dateNow = new Date();
-    private dateFrom: any;
-    hoveredDate: NgbDateStruct;
-    fromDate: NgbDateStruct;
-    toDate: NgbDateStruct;
-    show: boolean = false;
-
-    inputDate: string;
+    // model;
+    private dateTill;
+    private dateFrom;
+    private hoveredDate: NgbDateStruct;
+    private fromDate: NgbDateStruct;
+    private toDate: NgbDateStruct;
+    private show: boolean = false;
+    private inputDate: string;
   
     constructor(calendar: NgbCalendar) {
-      this.fromDate = calendar.getPrev(calendar.getToday(), 'd', 10);
-      this.toDate = calendar.getToday();
-      // console.log(this.toDate);
-      this.inputDate = formatDate(this.fromDate, this.toDate);
+      // this.fromDate = calendar.getPrev(calendar.getToday(), 'd', 10);
+      // this.toDate = calendar.getToday();
+      // this.inputDate = formatDate(this.fromDate, this.toDate);
     }
   
     onDateChange(date: NgbDateStruct) {
       if (!this.fromDate && !this.toDate) {
         this.fromDate = date;
+        this.dateFrom = convertDate(this.fromDate);
       } else if (this.fromDate && !this.toDate && after(date, this.fromDate)) {
         this.toDate = date;
+        this.dateTill = convertDate(this.toDate);
       } else {
         this.toDate = null;
         this.fromDate = date;
+        this.dateFrom = convertDate(this.fromDate);
       }
+      this.inputDate = formatDate(this.fromDate, this.toDate);
+      console.log(this.dateFrom, this.dateTill);
     }
   
   isHovered = date => this.fromDate && !this.toDate && this.hoveredDate && after(date, this.fromDate) && before(date, this.hoveredDate);
@@ -61,19 +65,29 @@ export class DateInputComponent {
 
     switch(val) {
       case '5 min':
-        this.dateFrom = setSeconds()- (1000*60*5);
+        this.dateTill = new Date().toISOString();
+        this.dateFrom = new Date(setSeconds() - (1000*60*5)).toISOString();
+        this.inputDate = 'last 5 min';
         break;
       case '1 hour':
-        this.dateFrom = setSeconds() - (1000*60*60);
+        this.dateTill = new Date().toISOString();
+        this.dateFrom = new Date(setSeconds() - (1000*60*60)).toISOString();
+        this.inputDate = 'last hour';
         break;
       case '6 hours':
-        this.dateFrom = setSeconds() - (1000*60*60*6);
+        this.dateTill = new Date().toISOString();
+        this.dateFrom = new Date(setSeconds() - (1000*60*60*6)).toISOString();
+        this.inputDate = 'last 6 hours';
         break;
       case '1 day':
-        this.dateFrom = setHours() - (1000*60*60*24);
+        this.dateTill = new Date().toISOString();
+        this.dateFrom = new Date(setHours() - (1000*60*60*24)).toISOString();
+        this.inputDate = 'last day';
         break;
       case '1 week':
-        this.dateFrom = setHours() - (1000*60*60*24*7);
+        this.dateTill = new Date().toISOString();
+        this.dateFrom = new Date(setHours() - (1000*60*60*24*7)).toISOString();
+        this.inputDate = 'last week';
         break;
     }
   }
@@ -88,7 +102,20 @@ function setSeconds() {
 }
 
 function formatDate(from: NgbDateStruct, till: NgbDateStruct) {
-  return 'From: ' + from.year + '-' + from.month + '-' + from.day +
-        ' | To: ' + till.year + '-' + till.month + '-' + till.day
+
+  if (!from || !till) {
+    return;
+  } else {
+    return 'From: ' + from.year + '-' + from.month + '-' + from.day +
+          ' | To: ' + till.year + '-' + till.month + '-' + till.day
+  }
+}
+
+function convertDate(obj: NgbDateStruct) {
+  let dateArr: number[] = [];
+  for(let key in obj) {
+    dateArr.push(obj[key]);
+  }
+  return new Date(dateArr[0], dateArr[1]-1, dateArr[2]).toISOString();
 }
 
